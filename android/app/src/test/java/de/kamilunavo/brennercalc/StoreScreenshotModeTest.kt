1d9aa062715b71621c5119b0c183f8f8e531eba6
Kamilunavo <acciento89@gmail.com>
2026-09-13 15:37:55 +0200

    Test namespaced store screenshot extra

--- android/app/src/test/java/de/kamilunavo/brennercalc/StoreScreenshotModeTest.kt
+++ android/app/src/test/java/de/kamilunavo/brennercalc/StoreScreenshotModeTest.kt
@@ -1,5 +1,6 @@
 package de.kamilunavo.brennercalc
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -7,6 +8,7 @@ import org.junit.Test
 class StoreScreenshotModeTest {
     @Test
     fun hidesBillingStatusOnlyForStoreScreenshots() {
+        assertEquals("de.kamilunavo.brennercalc.STORE_SCREENSHOTS", storeScreenshotsExtra("de.kamilunavo.brennercalc"))
         assertFalse(shouldShowBillingStatus(storeScreenshots = true))
         assertTrue(shouldShowBillingStatus(storeScreenshots = false))
     }
